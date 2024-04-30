55c2d260626115340fe68d6f6a1024d4d8a3e900
Lukas Heider Utimaco <lukas.heider.ext@utimaco.com>
2024-04-30 14:35:45 +0200

    Add toString, saveToFile and newDeckFromFile methods

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := newDeck()
-	hand, remainingCars := deal(cards, 5)
-	fmt.Println(hand, remainingCars)
+	cards := newDeckFromFile("cards")
+	cards.print()
 }
