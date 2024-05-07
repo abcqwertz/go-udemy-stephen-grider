f4653a3d37be41ba96c4577d3656d89361cf52a4
abcqwertz <lukas.heider.ext@utimaco.com>
2024-05-07 14:43:51 +0200

    Add tests and mod file

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 52 {
+		t.Errorf("NewDeck() returned %d cards, but wanted 52", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %s", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %s", d[len(d)-1])
+	}
+
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards, but got %d", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
+}
